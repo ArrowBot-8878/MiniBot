8152d2106bb6d89a6f33e30e7b8fdb4cbb7f628a
Isaac Patton <isaacpatton72@gmail.com>
2025-02-01 00:42:06 -0600

    Add pathplanner code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,6 +14,7 @@ import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.PS4Controller.Button;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
@@ -25,12 +26,16 @@ import frc.robot.subsystems.CANRollerSubsystem;
 import frc.robot.subsystems.DriveSubsystemOld;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 import java.util.List;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
 
 /*
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -42,6 +47,7 @@ public class RobotContainer {
   // The robot's subsystems
   private final DriveSubsystemOld m_robotDrive = new DriveSubsystemOld();
   private final CANRollerSubsystem rollerSubsystem = new CANRollerSubsystem();
+  SendableChooser<Command> autonChooser = new SendableChooser<Command>();
 
   // The driver's controller
   XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
@@ -53,6 +59,15 @@ public class RobotContainer {
    */
   public RobotContainer() {
     // Configure the button bindings
+
+
+    NamedCommands.registerCommand("RunIntake", new RollerCommand(()-> 0.5, ()-> 0, rollerSubsystem));
+    autonChooser.setDefaultOption("DefaultAuto", AutoBuilder.buildAuto("New Auto"));
+
+    
+
+
+
     configureButtonBindings();
 
     // Configure default commands
